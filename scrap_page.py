0e54b86f17f28e9971f1c393f81b6f01091587cc
alelorys <aleksandra.lorys@gmail.com>
2024-09-19 22:15:42 +0200

    starts new project

---
+++ scrap_page.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+'''
+    step 1: get content from link
+    step 2: seperate actual text from the rest
+    step 3: turn text into audio
+    step 4: save into file
+'''
