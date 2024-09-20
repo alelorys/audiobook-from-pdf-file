078e96236e9786701f6a3eea74b7fdb1bd6a07f0
alelorys <aleksandra.lorys@gmail.com>
2024-09-20 21:05:04 +0200

    scrape text from page and make audio

--- scrap_page.py
+++ scrap_page.py
@@ -1,4 +1,7 @@
 import requests
+import re
+from typing import List
+from gtts import gTTS
 from bs4 import BeautifulSoup
 
 '''
@@ -7,3 +10,25 @@ from bs4 import BeautifulSoup
     step 3: turn text into audio
     step 4: save into file
 '''
+def open_link(link:str) -> str:
+    page_content = requests.get(link)
+    return page_content.content
+
+def get_text(content) -> str:
+    soup = BeautifulSoup(content, 'html.parser')
+    return soup.get_text()
+
+def text_to_speech(content:str) -> gTTS:
+    lang = 'en'
+    return gTTS(content,lang=lang,slow=False)
+
+def save_audio(audio: gTTS, title):
+    audio.save(title)
+
+def main():
+    link = 'https://lwn.net/Articles/988894/'
+    text = get_text(open_link(link))
+    save_audio(text_to_speech(text),'test.mp3')
+
+if __name__ == "__main__":
+    main()
